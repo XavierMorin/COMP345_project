b470832f900155a4ba8d7f793f536e3151eb32f0
XavierMorin <56797234+XavierMorin@users.noreply.github.com>
2020-02-09 18:37:34 -0500

    Update 1

--- code/VGMap/VGMap.h
+++ code/VGMap/VGMap.h
@@ -1,14 +1,97 @@
-//
-// Created by Kunming Yang on 2020-01-22.
-//
+#pragma once
 
-#ifndef PROJECT_VGMAP_H
-#define PROJECT_VGMAP_H
+#ifndef VGMAP_H
+#define VGMAP_H
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+class Building;
+class BuildingSlot;
+class VGMap;
+
+class BuildingSlot {
+
+public:
+	//constructors && Destructor
+	BuildingSlot(); //Default
+	BuildingSlot(int Points_,int BuildingSlot_id_); //parameterized constructor with adjacent BuildingSlots
+	BuildingSlot(std::vector<BuildingSlot *> adjacentBuildings_, int Points_); //parameterized constructor with adjacent BuildingSlots
+	~BuildingSlot(); // Destructor
+	
+
+	// Accessors 
+	int& getPoints();    // Points accessor
+	int& getBuilding();  // Building accessor 
+
+	// Mutators
+	void setPoints(int size);  // Points mutator
+	//void setBuilding(Building * Building_);  //Building mutator
 
 
-class VGMap {
+
+	// adjacent BuildingSlot accessor method
+	std::vector<BuildingSlot *>& getAdjacentBuildingSlots();
+	// adjacent BuildingSlot mutator method
+	void setAdjacentbuildingSlots(std::vector<BuildingSlot *> adjBuildingSlot);
+	// add BuildingSlot to adjacent BuildingSlot vector method
+	void addAdjacentBuildingSlot(BuildingSlot *BuildingSlot);
+	// check if BuildingSlot is adjacent
+	bool isAdjacent(BuildingSlot *BuildingSlot);
+
+	
+	void print();
+
+
+
+private:
+	
+	
+	// vector of all adjacent BuildingSlot
+	std::vector<BuildingSlot *> *adjacentBuildingSlots;
+	
+	// Number of point for this Slot 
+	int *Points;
+	int *BuildingSlot_id;
+
+	std::string *BuildingBuilt; // to be changed to object Building
 
 };
+class VGMap {
 
 
-#endif //PROJECT_VGMAP_H
+public:
+	// Constructors && Destructor
+	VGMap();
+	VGMap(std::string name);// paramaterized constructor
+	VGMap(std::vector<BuildingSlot *> *BuildingSlot_,std::string name);// paramaterized constructor
+	~VGMap(); // destructor
+
+	// Accessor method
+	std::vector<BuildingSlot *>& getBuildingSlots();
+
+	// Mutator method
+	void  setBuildingSlots(std::vector<BuildingSlot *> BuildingSlot_);
+	
+	// add BuildingSlot method
+	void addBuildingSlot(BuildingSlot *BuildingSlot);
+
+	// PrintVGMap
+	void printVGMap();
+	// check if a map is a connected graph
+	bool isConnectedGraph();
+	// check if map contains duplicates
+	bool containsDuplicates();
+	// remove duplicates from map
+	void removeDuplicates();
+
+	void resetMap();
+
+private:
+	// vector containing pointers to all circular spaces of the VGMap
+	std::vector<BuildingSlot *> *BuildingSlots;
+	std::string *name;
+	
+};
+#endif 
