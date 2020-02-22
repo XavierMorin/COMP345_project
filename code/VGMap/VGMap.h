cd0b694f80c132c03f43692c646a886c183a2bb7
XavierMorin <56797234+XavierMorin@users.noreply.github.com>
2020-02-22 16:00:34 -0500

    Update VGMap.h

--- code/VGMap/VGMap.h
+++ code/VGMap/VGMap.h
@@ -6,7 +6,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
+#include "Resources.h"
 class Building;
 class BuildingSlot;
 class VGMap;
@@ -16,48 +16,50 @@ class BuildingSlot {
 public:
 	//constructors && Destructor
 	BuildingSlot(); //Default
-	BuildingSlot(int Points_,int BuildingSlot_id_); //parameterized constructor with adjacent BuildingSlots
-	BuildingSlot(std::vector<BuildingSlot *> adjacentBuildings_, int Points_); //parameterized constructor with adjacent BuildingSlots
+	BuildingSlot(int Points_, int BuildingSlot_id_); //parameterized constructor with adjacent BuildingSlots
+	BuildingSlot(std::vector<BuildingSlot *> adjacentBuildings_, int Points_, int BuildingSlot_id_); //parameterized constructor with adjacent BuildingSlots
 	~BuildingSlot(); // Destructor
-	
 
-	// Accessors 
+
+					 // Accessors 
 	int& getPoints();    // Points accessor
-	int& getBuilding();  // Building accessor 
+	Building& getBuilding();  // Building accessor 
 
-	// Mutators
+						 // Mutators
 	void setPoints(int size);  // Points mutator
-	//void setBuilding(Building * Building_);  //Building mutator
+							   //void setBuilding(Building * Building_);  //Building mutator
 
+	void setBuilding(Building b);
 
-
-	// adjacent BuildingSlot accessor method
+							   // adjacent BuildingSlot accessor method
 	std::vector<BuildingSlot *>& getAdjacentBuildingSlots();
 	// adjacent BuildingSlot mutator method
 	void setAdjacentbuildingSlots(std::vector<BuildingSlot *> adjBuildingSlot);
 	// add BuildingSlot to adjacent BuildingSlot vector method
-	void addAdjacentBuildingSlot(BuildingSlot *BuildingSlot);
+	void addAdjacentBuildingSlots(BuildingSlot *BuildingSlot);
 	// check if BuildingSlot is adjacent
 	bool isAdjacent(BuildingSlot *BuildingSlot);
+	int& getBuildingSlot_id();
 
-	
 	void print();
 
 
 
 private:
-	
-	
+
+
 	// vector of all adjacent BuildingSlot
 	std::vector<BuildingSlot *> *adjacentBuildingSlots;
-	
+
 	// Number of point for this Slot 
 	int *Points;
 	int *BuildingSlot_id;
 
-	std::string *BuildingBuilt; // to be changed to object Building
+	Building *BuildingBuilt; // to be changed to object Building
 
 };
+
+
 class VGMap {
 
 
@@ -65,15 +67,15 @@ public:
 	// Constructors && Destructor
 	VGMap();
 	VGMap(std::string name);// paramaterized constructor
-	VGMap(std::vector<BuildingSlot *> *BuildingSlot_,std::string name);// paramaterized constructor
+	VGMap(std::vector<BuildingSlot *> *BuildingSlot_, std::string name);// paramaterized constructor
 	~VGMap(); // destructor
 
-	// Accessor method
+			  // Accessor method
 	std::vector<BuildingSlot *>& getBuildingSlots();
 
 	// Mutator method
 	void  setBuildingSlots(std::vector<BuildingSlot *> BuildingSlot_);
-	
+
 	// add BuildingSlot method
 	void addBuildingSlot(BuildingSlot *BuildingSlot);
 
@@ -86,12 +88,13 @@ public:
 	// remove duplicates from map
 	void removeDuplicates();
 
-	void resetMap();
+	void resetVGMap();
 
 private:
 	// vector containing pointers to all circular spaces of the VGMap
 	std::vector<BuildingSlot *> *BuildingSlots;
 	std::string *name;
-	
+
 };
 #endif 
+
