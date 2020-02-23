4b80c019e8def93b459b3d70988ce56ef3b71324
XavierMorin <56797234+XavierMorin@users.noreply.github.com>
2020-02-22 20:15:14 -0500

    Update Resources.h

--- code/Resources/Resources.h
+++ code/Resources/Resources.h
@@ -3,73 +3,70 @@
 #define RRESOURCES_H
 #include <iostream>
 
-	class Tile;
-	class TileDeck;
-	class Building;
-	class BuildingDeck;
-	class Hand;
-	class Tile {
-		
-	
-	public:
-		Tile();
-		enum Types { sheep, wheet, stone, timber };
-		Tile(Types one, Types two, Types three, Types four);
-		~Tile();//
-		Types sToTypes(std::string s);
-		Types getUpLeft();
-		Types getUpRight();
-		Types getDownLeft();
-		Types getDownRight();
-		void setUpLeft(Types s);
-		void setUpRight(Types s);
-		void setDownLeft(Types s);
-		void setDownRight(Types s);
-		Types getRandomType();
-		void setNumOfResources();
-		Types* typeArray[4] = {upleft, upright,downright,downleft};
-		int numOfSheep();
-		int numOfWheet();
-		int numOfStone();
-		int numOfTimper();
-		void toString();
-		std::string typeToString(Types t);
-	private:
-		int* numOfResources[4] = {0,0,0,0}; //index0:sheep index1:wheet index2:stone index3:timber
-		Types* upleft;
-		Types* upright;
-		Types* downleft;
-		Types* downright;
-		
+class Tile;
+class TileDeck;
+class Building;
+class BuildingDeck;
+class Hand;
+class Tile {
 
-	};
-	class TileDeck {
-	private: Tile* allTiles[60];
-	public:
-		Tile* draw();
-		TileDeck();
-		~TileDeck();
-		
-	};
-class Building{
+
 public:
-    enum Type {sheep, stone, timber, wheet};
+	Tile();
+	enum Types { sheep, wheet, stone, timber };
+	Tile(Types one, Types two, Types three, Types four);
+	~Tile();
+	Types sToTypes(std::string s);
+	Types& getUpLeft();
+	Types& getUpRight();
+	Types& getDownLeft();
+	Types& getDownRight();
+	void setUpLeft(Types s);
+	void setUpRight(Types s);
+	void setDownLeft(Types s);
+	void setDownRight(Types s);
+	Types getRandomType();
+	void setNumOfResources();
+	void toString();
+
+private:
+	int* numOfResources[4] = { 0,0,0,0 }; //index0:sheep index1:wheet index2:stone index3:timber
+	Types* typeArray[4];
+	Types* upleft;
+	Types* upright;
+	Types* downleft;
+	Types* downright;
+
+};
+
+
+class TileDeck {
+private: Tile * allTiles[60];
+public:
+	Tile * draw();
+	TileDeck();
+	~TileDeck();
+
+};
+class Building {
+public:
+	enum Type { sheep, stone, timber, wheet };
 	Building();
 	Building(int num, Type t);
-	Building(int cost, Type type,bool s);
-    ~Building();
-	Type getType();
-	int getCost();
+	Building(int cost, Type type, bool s);
+	~Building();
+	Type& getType();
+	int& getCost();
 	void setType(Type type);
 	void setCost(int cost);
 	void setFlipped(bool s);
 	void turn();
-	bool getIsFlipped();
-	
+	bool& getIsFlipped();
+
 private:
 	bool* isFlipped;
-    int* cost;
-    Type* type;
+	int* cost;
+	Type* type;
 
 
 };
@@ -78,10 +75,12 @@ class BuildingDeck {
 	BuildingDeck();
 	Building* allBuilding[4][6];
 public:
-	Building* draw();
+	Building&  draw();
 
 };
-class Hand{
+
+
+class Hand {
 private:
 
 	int *numOfSheep;
@@ -93,10 +92,10 @@ public:
 	Hand();
 	Hand(int tree, int stone, int wheet, int timber);
 	//void exchange(GBMap gbmp);
-	int getNumOfSheep();
-	int getNumOfStone();
-	int getNumOfWheet();
-	int getNumOfTimber();
+	int& getNumOfSheep();
+	int& getNumOfStone();
+	int& getNumOfWheet();
+	int& getNumOfTimber();
 	void setNumOfSheep(int i);
 	void setNumOfTimber(int i);
 	void setNumOfStone(int i);
@@ -106,4 +105,5 @@ public:
 
 
 #endif // !RESOURCES_H
+
 
